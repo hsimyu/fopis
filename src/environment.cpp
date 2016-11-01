9edcae4e3683e799e513582364cdd24f4bb3316e
hsimyu <hsimyu1900@gmail.com>
2016-11-01 19:10:32 +0900

    Envをofstreamに渡した時の挙動を統一.

--- src/environment.cpp
+++ src/environment.cpp
@@ -19,6 +19,7 @@ std::ostream& operator<<(std::ostream& ost, const Environment& env){
     ost << "  iteration: " << env.max_iteration << std::endl;
     ost << " nx, ny, nz: " << format("%1%x%2%x%3%") % env.nx % env.ny % env.nz << std::endl;
     ost << "    process: " << format("%1%x%2%x%3%") % env.proc_x % env.proc_y % env.proc_z << std::endl;
-    ost << "       cell: " << format("%1%x%2%x%3%") % env.cell_x % env.cell_y % env.cell_z << std::endl;
+    ost << "       cell: " << format("%1%x%2%x%3%") % (env.cell_x - 2) % (env.cell_y - 2) % (env.cell_z - 2) << std::endl;
+    ost << "    cell(+): " << format("%1%x%2%x%3%") % env.cell_x % env.cell_y % env.cell_z << std::endl;
     return ost;
 }
