b9462db6a2baefc6d9e8ecb2cf2b21d18958a33a
hsimyu <hsimyu1900@gmail.com>
2016-12-19 17:13:43 +0900

    粒子情報出力の形式を微調整.

--- src/dataio.cpp
+++ src/dataio.cpp
@@ -37,8 +37,8 @@ namespace IO {
             ofs << "## " << env->ptype[id].getName() << endl;
 
             for(int i = 0; i < parray[id].size(); ++i){
-                ofs << format("%8.3f %8.3f %8.3f") % parray[id][i].getX() % parray[id][i].getY() % parray[id][i].getZ();
-                ofs << format("%8.3f %8.3f %8.3f") % parray[id][i].getVX() % parray[id][i].getVY() % parray[id][i].getVZ();
+                ofs << format("%9.4f %9.4f %9.4f") % parray[id][i].getX() % parray[id][i].getY() % parray[id][i].getZ();
+                ofs << format("%13.4e %13.4e %13.4e") % parray[id][i].getVX() % parray[id][i].getVY() % parray[id][i].getVZ();
                 ofs << endl;
             }
 
