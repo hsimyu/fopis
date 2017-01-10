f8af054ad9e447b680b1cfa456eee8ac1c99fbf4
hsimyu <hsimyu1900@gmail.com>
2017-01-10 19:28:23 +0900

    新規インストール時用のスクリプトをアップデート.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,11 @@ cd libraries
 
 # install silo
 wget https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2.tar.gz
+tar zxvf silo-4.10.2.tar.gz
+cd silo-4.10.2
+./configure --prefix=$HOME/local
+make
+make install
 
 cd ..
 rm -rf ./libraries
