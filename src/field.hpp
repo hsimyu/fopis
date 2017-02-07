1bfa72d4c9ed152e682bd9f0f6c3f314c6898e84
hsimyu <hsimyu1900@gmail.com>
2017-02-07 14:11:09 +0900

    Fieldクラスのinline化.

--- src/field.hpp
+++ src/field.hpp
@@ -5,7 +5,16 @@
 
 struct Poisson {
     public:
-        ~Poisson();
+        ~Poisson(void){
+            delete [] ipar;
+            delete [] dpar;
+            delete [] b_lx;
+            delete [] b_ly;
+            delete [] b_lz;
+            delete [] xhandle;
+            delete [] yhandle;
+            delete [] rho1D;
+        }
 
         MKL_INT nx;
         MKL_INT ny;
@@ -41,38 +50,106 @@ class Field {
 
         Poisson* psn = nullptr;
     public:
-        Field();
-        ~Field();
+        Field(void) :
+            rho(boost::extents[0][0][0], boost::fortran_storage_order()),
+            phi(boost::extents[0][0][0], boost::fortran_storage_order()),
+            ex(boost::extents[0][0][0], boost::fortran_storage_order()),
+            ey(boost::extents[0][0][0], boost::fortran_storage_order()),
+            ez(boost::extents[0][0][0], boost::fortran_storage_order()),
+            exref(boost::extents[0][0][0], boost::fortran_storage_order()),
+            eyref(boost::extents[0][0][0], boost::fortran_storage_order()),
+            ezref(boost::extents[0][0][0], boost::fortran_storage_order()),
+            bx(boost::extents[0][0][0], boost::fortran_storage_order()),
+            by(boost::extents[0][0][0], boost::fortran_storage_order()),
+            bz(boost::extents[0][0][0], boost::fortran_storage_order()) {}
 
-        // scalars
-        tdArray& getPhi();
-        void setPhi(tdArray&);
-        tdArray& getRho();
-        void setRho(tdArray&);
-        tdArray& getScalar(std::string);
+        // destructor
+        ~Field(){
+            delete psn;
+        }
 
-        // vectors
-        tdArray& getEx();
-        void setEx(tdArray&);
-        tdArray& getEy();
-        void setEy(tdArray&);
-        tdArray& getEz();
-        void setEz(tdArray&);
+        // potential
+        void setPhi(tdArray& _phi){
+            phi = _phi;
+        }
+        tdArray& getPhi(){
+            return phi;
+        }
 
-        // reference efield on nodes
-        tdArray& getExRef();
-        tdArray& getEyRef();
-        tdArray& getEzRef();
-        void setExRef(tdArray&);
-        void setEyRef(tdArray&);
-        void setEzRef(tdArray&);
+        // charge density
+        void setRho(tdArray& _rho){
+            rho = _rho;
+        }
+        tdArray& getRho(){
+            return rho;
+        }
 
-        tdArray& getBx();
-        void setBx(tdArray&);
-        tdArray& getBy();
-        void setBy(tdArray&);
-        tdArray& getBz();
-        void setBz(tdArray&);
+        tdArray& getScalar(std::string varname){
+            if(varname == "potential" || varname == "phi") {
+                return phi;
+            } else if(varname == "rho" || varname == "charge") {
+                return rho;
+            } else {
+                throw std::invalid_argument("[ERROR] Invalid varname is passed to getScalarField():" + varname);
+            }
+        }
+
+        // electric fields
+        void setEx(tdArray& _ex){
+            ex = _ex;
+        }
+        tdArray& getEx(){
+            return ex;
+        }
+
+        void setEy(tdArray& _ey){
+            ey = _ey;
+        }
+        tdArray& getEy(){
+            return ey;
+        }
+
+        void setEz(tdArray& _ez){
+            ez = _ez;
+        }
+        tdArray& getEz(){
+            return ez;
+        }
+
+        //! reference efield on nodes
+        tdArray& getExRef(){
+            return exref;
+        }
+
+        tdArray& getEyRef(){
+            return eyref;
+        }
+
+        tdArray& getEzRef(){
+            return ezref;
+        }
+
+        // magnetic fields
+        void setBx(tdArray& _bx){
+            bx = _bx;
+        }
+        tdArray& getBx(){
+            return bx;
+        }
+
+        void setBy(tdArray& _by){
+            by = _by;
+        }
+        tdArray& getBy(){
+            return by;
+        }
+
+        void setBz(tdArray& _bz){
+            bz = _bz;
+        }
+        tdArray& getBz(){
+            return bz;
+        }
 
         void initializePoisson(const int, const int, const int);
         void updateEfield(const int, const int, const int);
